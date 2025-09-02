80a7a8e6721a5ce26a2d456330488dc3e01b25d9
u23527685 <u23527685@tuks.co.za>
2025-09-02 13:32:03 +0200

    git setup2

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+module.exports = {
+    entry: "./frontend/src/index.js",
+    output: {
+        path: path.resolve("frontend","public"),
+        filename: "bundle.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+            }
+        ]
+    }
+}
